8ba6d51c305646b67083722552eb33605cee27a7
raphaelvaz <raphaeltrombetta@gmail.com>
2021-09-30 20:21:48 -0300

    feat: create form header

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,12 @@
+import { Container, FormArea } from './styles'
+
+export function Content() {
+    return (
+        <Container>
+            <FormArea>
+                <h2>Diário de saúde</h2>
+                <span>Crie seu relatório diário de saúde</span>
+            </FormArea>
+        </Container>
+    )
+}
