7aba02b7aa09a2d15c2a14dc1df60ea17174e623
raphaelvaz <raphaeltrombetta@gmail.com>
2021-09-30 17:27:43 -0300

    feat: add text span to aside component

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,12 +1,13 @@
 import { Container, Logo } from './styles'
 
 import logoImg from '../../assets/logo.svg'
-import runImg from '../../assets/run.png'
+import runImg from '../../assets/runningwoman.png'
 export function Aside() {
     return (
         <Container>
             <Logo src={logoImg} alt='logo' />
             <strong>Relatório de saúde em gráficos</strong>
+            <span>Em poucos segundos, transforme os seus dados de saúde em gráficos de fácil leitura.</span>
         </Container>
     )
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     height: 100vh;
-    min-width:450px;
+    min-width:500px;
 
     display: flex;
     flex-direction: column;
@@ -14,7 +14,15 @@ export const Container = styled.div`
         color: var(--asideText);
         font-size: 2.0rem;
         line-height: 2.5rem;
-        margin: 60px;
+        margin: 60px 0 25px 60px;
+    }
+    span{
+        color: var(--asideText);
+        line-height: 1.8rem;
+        max-width: 270px;
+        align-self: end;
+        margin-right: 15px;
+        line-break: normal;
     }
 `;
 
