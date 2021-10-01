1b08e06c69fa22145eb11b5722a9fc52f09f8000
raphaelvaz <raphaeltrombetta@gmail.com>
2021-09-30 21:48:20 -0300

    feat: add initial form fields

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -6,6 +6,20 @@ export function Content() {
             <FormArea>
                 <h2>Diário de saúde</h2>
                 <span>Crie seu relatório diário de saúde</span>
+                <form>
+                    <label htmlFor="name">Nome completo</label>
+                    <input name="name" type="text" />
+
+                    <label htmlFor="birthDate">Data de nascimento</label>
+                    <input name="birthDate" type="date" />
+
+                    <label htmlFor="date">Para qual dia você deseja gerar o gráfico de saúde?</label>
+                    <input name="date" type="date" />
+
+                    <label htmlFor="date">Selecione a hora para preencher os dados</label>
+
+                    <button type="submit">Gerar Diário de Saúde</button>
+                </form>
             </FormArea>
         </Container>
     )
