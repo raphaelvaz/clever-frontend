f1143e005b1e191a78b4d9766053d87c177b2126
raphaelvaz <raphaeltrombetta@gmail.com>
2021-10-03 17:57:54 -0300

    feat: add page Results to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Switch, Route } from 'react-router-dom'
 import { Register } from '../pages/Register'
+import { ResultsPage } from '../pages/ResultsPage'
 
 export function Routes() {
 
     return (
         <Switch>
             <Route path="/" exact component={Register} />
+            <Route path="/metrics" component={ResultsPage} />
         </Switch>
     )
 }
