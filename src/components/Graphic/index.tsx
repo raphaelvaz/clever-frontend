219f9824c2cfbea310b349f84e86899b0bf38d2f
raphaelvaz <raphaeltrombetta@gmail.com>
2021-10-03 19:50:43 -0300

    feat: create resultsPage header

--- src/components/Graphic/index.tsx
+++ src/components/Graphic/index.tsx
@@ -1,9 +1,34 @@
 import { Container } from './styles'
+import avatarImg from '../../assets/avatar.svg'
+import { Heart } from '../../assets/svgComponents/Heart'
+import { Group } from '../../assets/svgComponents/Group'
 
 export function Graphic() {
     return (
         <Container>
-            <h1>hello</h1>
+            <header>
+                <img src={avatarImg} alt='avatar' />
+                <strong>Maria José</strong>
+                <span> 39 anos</span>
+                <div>
+                    <span>Diário de Saúde</span>
+                    <span>14/09/2021</span>
+                </div>
+            </header>
+            <main>
+                <button>
+                    <div>
+                        <Heart fill={'#40465C'} />
+                        <strong>BPM</strong>
+                    </div>
+                </button>
+                <button>
+                    <div>
+                        <Group fill={'#40465C'} />
+                        <strong>PA</strong>
+                    </div>
+                </button>
+            </main>
         </Container>
     )
 }
