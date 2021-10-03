0b3e9c5d246cea48be5aacdb82f45632b209fb2d
raphaelvaz <raphaeltrombetta@gmail.com>
2021-10-03 17:57:25 -0300

    feat: create resultsPage

---
+++ src/components/Graphic/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles'
+
+export function Graphic() {
+    return (
+        <Container>
+            <h1>hello</h1>
+        </Container>
+    )
+}
---
+++ src/pages/ResultsPage/index.tsx
@@ -0,0 +1,15 @@
+import { Aside } from "../../components/Aside";
+import { Graphic } from "../../components/Graphic";
+import { Container } from "./styles";
+
+
+
+export function ResultsPage() {
+
+    return (
+        <Container>
+            <Aside />
+            <Graphic />
+        </Container>
+    )
+}
